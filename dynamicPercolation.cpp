c4b4ebf885e70aa895ea15fe1ef7e77e1bdbe09a
Konstantinos Angelou <26175143+akostas@users.noreply.github.com>
2017-12-29 12:26:31 +0200

    Update dynamicPercolation.cpp

--- dynamicPercolation.cpp
+++ dynamicPercolation.cpp
@@ -22,8 +22,8 @@ void DynCreation::insertEdge(string p, string q)
 void DynCreation::changeMax(int number)
 {
 	if( threemax[0] < number )	threemax[0] = number;
-    else if( threemax[1] < number )	threemax[1] = number;
-    else if( threemax[2] < number )	threemax[2] = number;
+	else if( threemax[1] < number )	threemax[1] = number;
+	else if( threemax[2] < number )	threemax[2] = number;
 }
 
 // If none of the nodes on the link exist
@@ -35,80 +35,80 @@ void DynCreation::noneExists(string p, string q)
 	clSize[clusNumber] += 2;
 	int tmpClSizeNocl = clSize[clusNumber];
 	tmp_i += (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
-  changeMax(tmpClSizeNocl);
+	changeMax(tmpClSizeNocl);
 }
 
 // If only the first node exists
 void DynCreation::onlyPExists(string p, string q)
 {
 	nocl[q] = nocl[p]; // Cited and citing nodes belong to the same cluster
-    int tmpClSizeNocl = clSize[nocl[p]];
-    tmp_i -= (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
-    // Increase cluster size by 1
-    clSize[nocl[p]] += 1;
-    tmpClSizeNocl = clSize[nocl[p]];
-    tmp_i += (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
-    changeMax(tmpClSizeNocl);
+	int tmpClSizeNocl = clSize[nocl[p]];
+	tmp_i -= (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
+	// Increase cluster size by 1
+	clSize[nocl[p]] += 1;
+	tmpClSizeNocl = clSize[nocl[p]];
+	tmp_i += (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
+	changeMax(tmpClSizeNocl);
 }
 
 // If only the second node exists
 void DynCreation::onlyQExists(string p, string q)
 {
 	nocl[p]=nocl[q];    // Cited and citing nodes belong to the same cluster
-    int tmpClSizeNocl = clSize[nocl[q]];
-    tmp_i -= (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
-    // Increase cluster size by 1
-    clSize[nocl[q]] += 1;
-    tmpClSizeNocl = clSize[nocl[q]];
-    tmp_i += (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
-    changeMax(tmpClSizeNocl);
+	int tmpClSizeNocl = clSize[nocl[q]];
+	tmp_i -= (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
+	// Increase cluster size by 1
+	clSize[nocl[q]] += 1;
+	tmpClSizeNocl = clSize[nocl[q]];
+	tmp_i += (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
+	changeMax(tmpClSizeNocl);
 }
 
 // If both nodes exist
 void DynCreation::bothExist(string p, string q)
 {
 	// Check if both nodes belong already to the same cluster
-    if( nocl[q] == nocl[p] )    return;
-    int temp1=nocl[p];
-    int temp2=nocl[q];
-    int tmpClSizeNocl = clSize[temp1];
-    tmp_i -= (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
-    tmpClSizeNocl = clSize[temp2];
-    tmp_i -= (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
+	if( nocl[q] == nocl[p] )    return;
+	int temp1=nocl[p];
+	int temp2=nocl[q];
+	int tmpClSizeNocl = clSize[temp1];
+	tmp_i -= (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
+	tmpClSizeNocl = clSize[temp2];
+	tmp_i -= (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
 	int temp3 = 0, temp4 = 0;
 	// Merge the two clusters, keeping(removing) the largest(smallest)
 	if( clSize[temp1] >= clSize[temp2] )
-    {
-    	for(auto& x: nocl)
-        {
-            if(x.second == temp2)
-            {
-                x.second = temp1;
-                clSize[temp2]--;
-                clSize[temp1]++;
-            }
-        }
-        temp3 = temp1;	// Largest
-        temp4 = temp2;
+    	{
+		for(auto& x: nocl)
+		{
+		    if(x.second == temp2)
+		    {
+			x.second = temp1;
+			clSize[temp2]--;
+			clSize[temp1]++;
+		    }
+		}
+		temp3 = temp1;	// Largest
+		temp4 = temp2;
 	}
 	else
 	{
 		for(auto& x: nocl)
-        {
-            if(x.second == temp1)
-            {
-                x.second = temp2;
-                clSize[temp1]--;
-                clSize[temp2]++;
-            }
-        }
-        temp3 = temp2;	// Largest
-        temp4 = temp1;
+		{
+		    if(x.second == temp1)
+		    {
+			x.second = temp2;
+			clSize[temp1]--;
+			clSize[temp2]++;
+		    }
+		}
+		temp3 = temp2;	// Largest
+		temp4 = temp1;
 	}
-    tmpClSizeNocl = clSize[temp3];
-    tmp_i += (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
-    changeMax(tmpClSizeNocl);
-    clSize.erase(temp4);
+	tmpClSizeNocl = clSize[temp3];
+	tmp_i += (long long int)tmpClSizeNocl*(long long int)tmpClSizeNocl;
+	changeMax(tmpClSizeNocl);
+	clSize.erase(temp4);
 }
 
 // Calculate reduced average cluster size
@@ -137,8 +137,8 @@ void DynCreation::mProcess(string p, string q)
 	insertEdge(p,q);
 	bool pExists = nocl.find(p) != nocl.end();
 	bool qExists = nocl.find(q) != nocl.end();
-	if( !pExists and !qExists )		noneExists(p, q);
+	if( !pExists and !qExists )	noneExists(p, q);
 	else if( pExists and !qExists )	onlyPExists(p, q);
 	else if( !pExists and qExists )	onlyQExists(p, q);
-	else if( pExists and qExists )		bothExist(p, q);
+	else if( pExists and qExists )	bothExist(p, q);
 }
